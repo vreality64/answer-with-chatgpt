aeb0c6d702b43d83658421ca51bc5e2238ba82ee
vreality64 <sh.park@toss.im>
2023-02-13 21:48:30 +0900

    refactor: replace build directory name

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
-    outDir: 'build',
+    outDir: 'bin',
     target: 'node16',
     platform: 'node',
     format: ['esm'],
